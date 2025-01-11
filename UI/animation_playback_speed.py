7962967ac798e37a0d5fe56103574839e230f6db
hayden <haydenguyn@gmail.com>
2025-01-10 19:43:25 -0800

    Updated radio buttons to have relevant information and added slider

--- UI/animation_playback_speed.py
+++ UI/animation_playback_speed.py
@@ -16,62 +16,73 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QHBoxLayout, QMainWindow, QMenuBar,
-    QRadioButton, QSizePolicy, QStatusBar, QVBoxLayout,
-    QWidget)
+    QRadioButton, QSizePolicy, QSlider, QStatusBar,
+    QVBoxLayout, QWidget)
 
-class Ui_animation_playback_speed(object):
-    def setupUi(self, animation_playback_speed):
-        if not animation_playback_speed.objectName():
-            animation_playback_speed.setObjectName(u"animation_playback_speed")
-        animation_playback_speed.resize(800, 600)
-        self.centralwidget = QWidget(animation_playback_speed)
+class Ui_main_window(object):
+    def setupUi(self, main_window):
+        if not main_window.objectName():
+            main_window.setObjectName(u"main_window")
+        main_window.resize(94, 318)
+        self.centralwidget = QWidget(main_window)
         self.centralwidget.setObjectName(u"centralwidget")
         self.horizontalLayout = QHBoxLayout(self.centralwidget)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.verticalLayout = QVBoxLayout()
         self.verticalLayout.setObjectName(u"verticalLayout")
-        self.radioButton_2 = QRadioButton(self.centralwidget)
-        self.radioButton_2.setObjectName(u"radioButton_2")
+        self.button_05x = QRadioButton(self.centralwidget)
+        self.button_05x.setObjectName(u"button_05x")
 
-        self.verticalLayout.addWidget(self.radioButton_2)
+        self.verticalLayout.addWidget(self.button_05x)
 
-        self.radioButton = QRadioButton(self.centralwidget)
-        self.radioButton.setObjectName(u"radioButton")
+        self.button_1x = QRadioButton(self.centralwidget)
+        self.button_1x.setObjectName(u"button_1x")
 
-        self.verticalLayout.addWidget(self.radioButton)
+        self.verticalLayout.addWidget(self.button_1x)
 
-        self.radioButton_3 = QRadioButton(self.centralwidget)
-        self.radioButton_3.setObjectName(u"radioButton_3")
+        self.button_025x = QRadioButton(self.centralwidget)
+        self.button_025x.setObjectName(u"button_025x")
 
-        self.verticalLayout.addWidget(self.radioButton_3)
+        self.verticalLayout.addWidget(self.button_025x)
 
-        self.radioButton_4 = QRadioButton(self.centralwidget)
-        self.radioButton_4.setObjectName(u"radioButton_4")
+        self.button2x = QRadioButton(self.centralwidget)
+        self.button2x.setObjectName(u"button2x")
 
-        self.verticalLayout.addWidget(self.radioButton_4)
+        self.verticalLayout.addWidget(self.button2x)
+
+        self.verticalLayout_2 = QVBoxLayout()
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.slider = QSlider(self.centralwidget)
+        self.slider.setObjectName(u"slider")
+        self.slider.setOrientation(Qt.Orientation.Vertical)
+
+        self.verticalLayout_2.addWidget(self.slider, 0, Qt.AlignmentFlag.AlignHCenter)
+
+
+        self.verticalLayout.addLayout(self.verticalLayout_2)
 
 
         self.horizontalLayout.addLayout(self.verticalLayout)
 
-        animation_playback_speed.setCentralWidget(self.centralwidget)
-        self.menubar = QMenuBar(animation_playback_speed)
+        main_window.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(main_window)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 800, 23))
-        animation_playback_speed.setMenuBar(self.menubar)
-        self.statusbar = QStatusBar(animation_playback_speed)
+        self.menubar.setGeometry(QRect(0, 0, 94, 23))
+        main_window.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(main_window)
         self.statusbar.setObjectName(u"statusbar")
-        animation_playback_speed.setStatusBar(self.statusbar)
+        main_window.setStatusBar(self.statusbar)
 
-        self.retranslateUi(animation_playback_speed)
+        self.retranslateUi(main_window)
 
-        QMetaObject.connectSlotsByName(animation_playback_speed)
+        QMetaObject.connectSlotsByName(main_window)
     # setupUi
 
-    def retranslateUi(self, animation_playback_speed):
-        animation_playback_speed.setWindowTitle(QCoreApplication.translate("animation_playback_speed", u"MainWindow", None))
-        self.radioButton_2.setText(QCoreApplication.translate("animation_playback_speed", u"RadioButton", None))
-        self.radioButton.setText(QCoreApplication.translate("animation_playback_speed", u"RadioButton", None))
-        self.radioButton_3.setText(QCoreApplication.translate("animation_playback_speed", u"RadioButton", None))
-        self.radioButton_4.setText(QCoreApplication.translate("animation_playback_speed", u"RadioButton", None))
+    def retranslateUi(self, main_window):
+        main_window.setWindowTitle(QCoreApplication.translate("main_window", u"Playback Speed", None))
+        self.button_05x.setText(QCoreApplication.translate("main_window", u"1x", None))
+        self.button_1x.setText(QCoreApplication.translate("main_window", u"0.5x", None))
+        self.button_025x.setText(QCoreApplication.translate("main_window", u"0.25x", None))
+        self.button2x.setText(QCoreApplication.translate("main_window", u"2x", None))
     # retranslateUi
 
