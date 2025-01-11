f422721af5f906e6038c5387ad44236799727085
hayden <haydenguyn@gmail.com>
2025-01-10 19:43:05 -0800

    Updated name of imports

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import sys
-import json
 from maya import cmds
 
 sys.path.append("/home/hayden/Documents/Maya/Animation_Playback_Speed")
 
 from PySide6.QtWidgets import QMainWindow, QApplication
-import UI.animation_playback_speed as a_s_p
+import UI.animation_playback_speed as aps
 
-class Animation_Playback_Speed(QMainWindow, a_s_p.Ui_animation_playback_speed):
+class Animation_Playback_Speed(QMainWindow, aps.Ui_main_window):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
