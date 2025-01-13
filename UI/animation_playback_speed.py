35bdd3c421eef11e90b197a71271a492dcda98bf
hayden <haydenguyn@gmail.com>
2025-01-13 14:25:45 -0800

    Updated layouts in .ui file

--- UI/animation_playback_speed.py
+++ UI/animation_playback_speed.py
@@ -15,54 +15,69 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QHBoxLayout, QMainWindow, QMenuBar,
-    QRadioButton, QSizePolicy, QSlider, QStatusBar,
-    QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QHBoxLayout, QLabel, QMainWindow,
+    QMenuBar, QRadioButton, QSizePolicy, QSlider,
+    QStatusBar, QVBoxLayout, QWidget)
 
 class Ui_main_window(object):
     def setupUi(self, main_window):
         if not main_window.objectName():
             main_window.setObjectName(u"main_window")
         main_window.resize(94, 318)
+        icon = QIcon(QIcon.fromTheme(QIcon.ThemeIcon.MediaPlaybackStart))
+        main_window.setWindowIcon(icon)
         self.centralwidget = QWidget(main_window)
         self.centralwidget.setObjectName(u"centralwidget")
         self.horizontalLayout = QHBoxLayout(self.centralwidget)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.verticalLayout = QVBoxLayout()
-        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.v_layout = QVBoxLayout()
+        self.v_layout.setObjectName(u"v_layout")
+        self.v_button_layout = QVBoxLayout()
+        self.v_button_layout.setObjectName(u"v_button_layout")
         self.button_1x = QRadioButton(self.centralwidget)
         self.button_1x.setObjectName(u"button_1x")
 
-        self.verticalLayout.addWidget(self.button_1x)
+        self.v_button_layout.addWidget(self.button_1x)
 
         self.button_05x = QRadioButton(self.centralwidget)
         self.button_05x.setObjectName(u"button_05x")
 
-        self.verticalLayout.addWidget(self.button_05x)
+        self.v_button_layout.addWidget(self.button_05x)
 
         self.button_025x = QRadioButton(self.centralwidget)
         self.button_025x.setObjectName(u"button_025x")
 
-        self.verticalLayout.addWidget(self.button_025x)
+        self.v_button_layout.addWidget(self.button_025x)
 
         self.button_2x = QRadioButton(self.centralwidget)
         self.button_2x.setObjectName(u"button_2x")
 
-        self.verticalLayout.addWidget(self.button_2x)
+        self.v_button_layout.addWidget(self.button_2x)
 
-        self.verticalLayout_2 = QVBoxLayout()
-        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+
+        self.v_layout.addLayout(self.v_button_layout)
+
+        self.v_slider_layout = QVBoxLayout()
+        self.v_slider_layout.setObjectName(u"v_slider_layout")
         self.slider = QSlider(self.centralwidget)
         self.slider.setObjectName(u"slider")
+        self.slider.setMaximum(10)
         self.slider.setOrientation(Qt.Orientation.Vertical)
+        self.slider.setTickPosition(QSlider.TickPosition.NoTicks)
+        self.slider.setTickInterval(1)
 
-        self.verticalLayout_2.addWidget(self.slider, 0, Qt.AlignmentFlag.AlignHCenter)
+        self.v_slider_layout.addWidget(self.slider, 0, Qt.AlignmentFlag.AlignHCenter)
+
+        self.label = QLabel(self.centralwidget)
+        self.label.setObjectName(u"label")
+
+        self.v_slider_layout.addWidget(self.label, 0, Qt.AlignmentFlag.AlignHCenter)
 
 
-        self.verticalLayout.addLayout(self.verticalLayout_2)
+        self.v_layout.addLayout(self.v_slider_layout)
 
 
-        self.horizontalLayout.addLayout(self.verticalLayout)
+        self.horizontalLayout.addLayout(self.v_layout)
 
         main_window.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(main_window)
@@ -79,10 +94,11 @@ class Ui_main_window(object):
     # setupUi
 
     def retranslateUi(self, main_window):
-        main_window.setWindowTitle(QCoreApplication.translate("main_window", u"Playback Speed", None))
+        main_window.setWindowTitle("")
         self.button_1x.setText(QCoreApplication.translate("main_window", u"1x", None))
         self.button_05x.setText(QCoreApplication.translate("main_window", u"0.5x", None))
         self.button_025x.setText(QCoreApplication.translate("main_window", u"0.25x", None))
         self.button_2x.setText(QCoreApplication.translate("main_window", u"2x", None))
+        self.label.setText(QCoreApplication.translate("main_window", u"0x", None))
     # retranslateUi
 
